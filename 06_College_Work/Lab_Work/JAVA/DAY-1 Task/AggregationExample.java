4c5c4862819daaa909a5f8d25fc8131a168779f5
alex46x <alexdon9990@gmail.com>
2026-02-08 10:09:39 +0600

    Notebook Update

---
+++ 06_College_Work/Lab_Work/JAVA/DAY-1 Task/AggregationExample.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Professor {
+    private String name;
+    private String subject;
+    
+    public Professor(String name, String subject) {
+        this.name = name;
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void teach() {
+        System.out.println(name + " is teaching " + subject);
+    }
+}
+
+class University {
+    private String universityName;
+    private List<Professor> professors;
+
+    public University(String universityName) {
+        this.universityName = universityName;
+        this.professors = new ArrayList<>();
+    }
+
+    public void addProfessor(Professor professor) {
+        professors.add(professor);
+    }
+
+    public void showProfessors() {
+        System.out.println("Professors at " + universityName + ":");
+        for (Professor professor : professors) {
+            System.out.println(" - " + professor.getName());
+        }
+    }
+}
+
+public class AggregationExample {
+    public static void main(String[] args) {
+        Professor prof1 = new Professor("Dr. Smith", "Computer Science");
+        Professor prof2 = new Professor("Dr. Johnson", "Mathematics");
+
+        University university = new University("Harvard University");
+        university.addProfessor(prof1);
+        university.addProfessor(prof2);
+
+        university.showProfessors();
+
+        prof1.teach();
+        prof2.teach();
+    }
+}
---
+++ 06_College_Work/Lab_Work/JAVA/DAY-1 Task/Loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Loop {
+    public static void main(String[] args) {
+        
+        for(int i = 0 ; i<5;i++){
+            System.err.println(i);
+        }
+    }
+}

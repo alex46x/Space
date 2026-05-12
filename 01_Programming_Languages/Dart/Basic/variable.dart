716243d36758c780eabcfc65e5da7efaa2a43bd2
alex46x <alexdon9990@gmail.com>
2026-05-12 20:47:29 +0600

    Start dart

---
+++ 01_Programming_Languages/Dart/Basic/variable.dart
@@ -0,0 +1,18 @@
+void main() {
+  print("Hello World");
+  int x = 5;
+  double y = 3.14;
+  String name = "Dart";
+  print("Integer: $x");
+  print("Double: $y");
+  print("String: $name");
+  var z = "This is a variable with var";
+  print(z);
+
+  print("Normal print ");
+  print("This is a print with a newline\n");
+  print("This is a print with a tab\tand more text");
+  print(x);
+  print(y);
+  print(name);
+}
